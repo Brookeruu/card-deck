a6dc1102eb3b89e1b9ff89b6ce16ecad68ec81ad
Brooke Perry <perrysbrooke@gmail.com>
2018-10-22 20:53:21 -0700

    update html id and class, add js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
 $(function(){
-  $("#cardeck").submit(function(event){
+  $("#card-deck").submit(function(event){
     event.preventDefault();
 
-    var numbers = ["2","3","4","5"]
-    var suits = ["hearts","spades","clubs","diamonds",]
-    var output = [];
-    numbers.forEach(function(number){
-var input = $(number + suits).val();
-    output.forEach(function(element){
-      // alert("spa" + spade);
-      $(".clubs").append("<li>" + element + "</li>");
+    var numbers = ["2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"]
+    var suits = ["Hearts","Spades","Clubs","Diamonds",]
+    var output = []
 
+// this will return the list in order by numbers
+
+//     numbers.forEach(function(number){
+//       suits.forEach(function(suit){
+//         output.push(number + " of " + suit);
+//         $(".card-list").append("<li>" + number + " of " + suit + "</li>");
+//       });
+//     });
+//   });
+// });
+
+// this will return the list in order by suit
+    suits.forEach(function(suit){
+      numbers.forEach(function(number){
+        output.push(number + " of " + suit);
+        $(".card-list").append("<li>" + number + " of " + suit + "</li>");
+      });
     });
   });
 });
-});
+
+
+
+// output.forEach(function(element){
+//   // alert("spa" + spade);
+//   $(".clubs").append("<li>" + element + "</li>");
